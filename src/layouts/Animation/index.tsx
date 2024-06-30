95c8e8edef7a31ee43b9d452afc55f384ed20895
dr.cat911 <dr.cat911@generative.xyz>
2024-06-30 21:49:04 +0700

    add back event

--- src/layouts/Animation/index.tsx
+++ src/layouts/Animation/index.tsx
@@ -1,64 +1,72 @@
 'use client';
 
-import { useGSAP } from '@gsap/react';
-import { isPlayState } from '@Layouts/Animation/animationSignal';
+import {useGSAP} from '@gsap/react';
+import {isPlayState} from '@Layouts/Animation/animationSignal';
 import PageLoader from '@Layouts/PageLoader';
-import { useSignalEffect } from '@preact/signals-react';
-import { gsap } from 'gsap';
-import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { PropsWithChildren, ReactElement, useEffect, useRef } from 'react';
+import {useSignalEffect} from '@preact/signals-react';
+import {gsap} from 'gsap';
+import {ScrollToPlugin} from 'gsap/dist/ScrollToPlugin';
+import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
+import {PropsWithChildren, ReactElement, useEffect, useRef} from 'react';
 
 import useWindowResize from '@/hooks/useWindowResize';
 
-interface IProp extends PropsWithChildren {}
-
-if (typeof window !== 'undefined') {
-  history.scrollRestoration = 'manual';
+interface IProp extends PropsWithChildren {
 }
 
-export default function Animate({ children }: IProp): ReactElement {
-  const { scrollHeight } = useWindowResize();
-  const mainRef = useRef<HTMLElement>(null);
-
-  const { contextSafe } = useGSAP(() => {
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-  });
-  const onRefresh = contextSafe(() => {
-    mainRef.current && ScrollTrigger.refresh();
-  });
-
-  useSignalEffect(() => {
-    scrollHeight.value && isPlayState.value && onRefresh();
-  });
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
+if (typeof window !== 'undefined') {
     history.scrollRestoration = 'manual';
+}
 
-    ScrollTrigger.config({
-      ignoreMobileResize: true,
+export default function Animate({children}: IProp): ReactElement {
+    const {scrollHeight} = useWindowResize();
+    const mainRef = useRef<HTMLElement>(null);
+
+    const {contextSafe} = useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+    });
+    const onRefresh = contextSafe(() => {
+        mainRef.current && ScrollTrigger.refresh();
     });
 
-    const removeSpaceDown = (e: KeyboardEvent): void => {
-      if (
-          (e.keyCode === 32 || e.keyCode === 38 || e.keyCode === 40) &&
-          e.target === document.body
-      ) {
-        e.preventDefault();
-      }
-    };
+    useSignalEffect(() => {
+        scrollHeight.value && isPlayState.value && onRefresh();
+    });
 
-    window.addEventListener('keydown', removeSpaceDown);
-    return () => {
-      window.removeEventListener('keydown', removeSpaceDown);
-    };
-  }, []);
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, useGSAP);
+        history.scrollRestoration = 'manual';
 
-  return (
-      <main ref={mainRef}>
-        <PageLoader />
-        {children}
-      </main>
-  );
+        ScrollTrigger.config({
+            ignoreMobileResize: true,
+        });
+
+        const removeSpaceDown = (e: KeyboardEvent): void => {
+            if (
+                (e.keyCode === 32 || e.keyCode === 38 || e.keyCode === 40) &&
+                e.target === document.body
+            ) {
+                e.preventDefault();
+            }
+        };
+
+        window.addEventListener('keydown', removeSpaceDown);
+        return () => {
+            window.removeEventListener('keydown', removeSpaceDown);
+        };
+    }, []);
+
+    useEffect(() => {
+        window.onpopstate = (window.history as any).onpushstate = function () {
+            // toggleState.value = true;
+            // toggleState transition
+        }
+    }, []);
+
+    return (
+        <main ref={mainRef}>
+            <PageLoader/>
+            {children}
+        </main>
+    );
 }
