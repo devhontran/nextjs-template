71a896785dafb92ced0327757892c6f3eb1f3ba4
devhontran <dev.hontran@gmail.com>
2024-07-30 23:58:11 +0700

    add glitch text hovers

--- src/interactive/TextGlitch/index.tsx
+++ src/interactive/TextGlitch/index.tsx
@@ -1,9 +1,87 @@
 'use client';
 
-import { cloneElement, isValidElement, PropsWithChildren, ReactElement, useRef } from 'react';
+import {
+  cloneElement,
+  isValidElement,
+  PropsWithChildren,
+  ReactElement,
+  useEffect,
+  useRef,
+} from 'react';
 
-export default function InteractiveTextGlitch({ children }: PropsWithChildren): ReactElement {
+export enum TextGlitch {
+  auto_stop = 'auto_stop',
+  leave_stop = 'leave_stop',
+  step_stop = 'step_stop',
+}
+
+interface IProps extends PropsWithChildren {
+  type?: TextGlitch;
+}
+
+export default function InteractiveTextGlitch({
+  children,
+  type = TextGlitch.auto_stop,
+}: IProps): ReactElement {
   const refContent = useRef<HTMLDivElement>(null);
+  const refTextContent = useRef<string>('');
+  const refTime = useRef<NodeJS.Timeout | null>(null);
+  const refTimAutoStop = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    const isLeaveStop = type === TextGlitch.leave_stop;
+    const isAutoStop = type === TextGlitch.auto_stop;
+    const isStepStop = type === TextGlitch.step_stop;
+
+    if (!refContent.current) return;
+    refTextContent.current = refContent.current.textContent || '';
+
+    const text = '12345667890';
+    const onEnter = (): void => {
+      refTime.current && clearInterval(refTime.current);
+      isAutoStop && refTimAutoStop.current && clearTimeout(refTimAutoStop.current);
+      const currentText = refTextContent.current.split('');
+
+      let index = -1;
+
+      refTime.current = setInterval(() => {
+        for (let i = Math.floor(index); i < currentText.length; i++) {
+          if (currentText[i] !== ' ') {
+            currentText[i] = text[Math.floor(Math.random() * text.length)];
+          }
+        }
+
+        if (isStepStop) {
+          currentText[Math.floor(index)] = refTextContent.current[Math.floor(index)];
+          index += 0.3;
+          if (Math.floor(index) >= refTextContent.current.length) {
+            refTime.current && clearInterval(refTime.current);
+          }
+        }
+        if (refContent.current) refContent.current.textContent = currentText.join('');
+      }, 40);
+
+      if (isAutoStop) {
+        refTimAutoStop.current = setTimeout(() => {
+          refTime.current && clearInterval(refTime.current);
+          if (refContent.current) refContent.current.textContent = refTextContent.current;
+        }, 600);
+      }
+    };
+    const onLeave = (): void => {
+      refTime.current && clearInterval(refTime.current);
+      if (refContent.current) refContent.current.textContent = refTextContent.current;
+    };
+
+    refContent.current?.addEventListener('mouseenter', onEnter);
+    isLeaveStop && refContent.current?.addEventListener('mouseleave', onLeave);
+
+    return () => {
+      refContent.current?.removeEventListener('mouseenter', onEnter);
+      isLeaveStop && refContent.current?.removeEventListener('mouseleave', onLeave);
+    };
+  }, []);
+
   if (!isValidElement(children)) {
     return <div>Error: Invalid children element</div>;
   }
--- src/modules/InteractivePage/index.tsx
+++ src/modules/InteractivePage/index.tsx
@@ -2,6 +2,7 @@
 
 import MotionPreview from '@Components/MotionPreview';
 import InteractiveCharsScale from '@Interactive/CharsScale';
+import InteractiveTextGlitch, { TextGlitch } from '@Interactive/TextGlitch';
 import InteractiveVariable from '@Interactive/Variable';
 
 import s from './styles.module.scss';
@@ -22,6 +23,22 @@ export default function InteractivePage(): JSX.Element {
             <InteractiveCharsScale>
               <h1 className={s.heading}>Lorem Ipsum</h1>
             </InteractiveCharsScale>
+          </MotionPreview>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-5">
+        <div className="col-span-1">
+          <MotionPreview heading={'Interactive Text Glitch: Auto Stop'}>
+            <InteractiveTextGlitch>
+              <h1 className={s.heading}>Lorem Ipsum</h1>
+            </InteractiveTextGlitch>
+          </MotionPreview>
+        </div>
+        <div className="col-span-1">
+          <MotionPreview heading={'Interactive Text Glitch: Step and Stop'}>
+            <InteractiveTextGlitch type={TextGlitch.step_stop}>
+              <h1 className={s.heading}>Lorem Ipsum</h1>
+            </InteractiveTextGlitch>
           </MotionPreview>
         </div>
       </div>
