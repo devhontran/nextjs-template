032683af1b3e0403d39f3afc105a598463f346ad
0xd22f9c <dev.hontran@gmail.com>
2024-12-31 15:16:06 +0700

    Enhance Footer component styling and animation integration
    - Updated Footer component to include MotionLinesType for improved animation handling.
    - Modified heading styles in the Footer to increase font size and change color for better visibility.
    - Enhanced the overall visual presentation of the Footer, aligning with recent design updates.
    
    This commit improves the aesthetic and functional aspects of the Footer component, contributing to a more engaging user experience.

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -2,16 +2,18 @@
 
 import React from 'react';
 
-import MotionLines from '@/animation/components/Typo/Lines';
+import MotionLines, { MotionLinesType } from '@/animation/components/Typo/Lines';
 
 import s from './styles.module.scss';
 
 export default function Footer(): React.ReactElement {
   return (
     <footer className={s.footer}>
-      <h1>FOOTER</h1>
+      <h1 style={{ fontSize: '55px', color: 'white' }}>FOOTER</h1>
       <MotionLines motion={{ delayEnter: 0.2 }} type={MotionLinesType.fade}>
-        <h1 className={s.heading}>This is the Home Page</h1>
+        <h1 className={s.heading} style={{ fontSize: '55px', color: 'white' }}>
+          This is the Home Page
+        </h1>
       </MotionLines>
     </footer>
   );
