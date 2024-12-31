06c4ddbc90fd2a251b5aae2fd1ecbe84c4177e3d
0xd22f9c <dev.hontran@gmail.com>
2024-12-31 16:01:44 +0700

    Enhance animation components with ScrollTrigger integration and refactor ImagePlaceHolder
    - Registered ScrollTrigger plugin in MotionImageParallax for improved scroll-based animations.
    - Updated MotionChars to utilize words instead of chars for better animation handling.
    - Refactored ImagePlaceHolder component by removing unused state management and simplifying the structure.
    - Adjusted styles in ImagePlaceHolder to improve layout and object-fit properties.
    
    This commit improves animation performance and code organization, contributing to a more efficient user experience.

--- src/components/ImagePlaceHolder/index.tsx
+++ src/components/ImagePlaceHolder/index.tsx
@@ -1,45 +1,31 @@
 'use client';
 
-import { useSignal, useSignalEffect } from '@preact/signals-react';
 import Image, { ImageProps } from 'next/image';
-import { ReactElement, useRef, useState } from 'react';
+import { ReactElement, useRef } from 'react';
 
-// import useImagePreloader from '@/animation/useImagePreloader';
 import s from './style.module.scss';
 
 const ImagePlaceHolder = (props: ImageProps): ReactElement => {
-  const [isReady, setIsReady] = useState<boolean>(false);
-  const isLoaded = useSignal<boolean>(false);
   const refPlaceImg = useRef<HTMLImageElement>(null);
-  // const onLoaded = useImagePreloader(refPlaceImg);
-  const { className, width, alt, src } = props;
+  // const refOriginImg = useRef<HTMLImageElement>(null);
 
-  useSignalEffect(() => {
-    isLoaded.value && refPlaceImg.current?.classList.add(s.isLoaded);
-  });
+  const { className, alt, src } = props;
 
   return (
     <div className={`${s.imagePreload} imagePreload`}>
-      {isReady && (
-        <Image
-          className={`${className} ${s.imagePreload_origin}`}
-          onLoad={(): void => {
-            isLoaded.value = true;
-          }}
-          quality={100}
-          sizes={`${width ? `(max-width: ${width}px) 100vw, ${width}px` : '100vw'}`}
-          {...props}
-          alt={alt}
-        />
-      )}
+      {/* <Image
+        className={`${className} ${s.imagePreload_origin}`}
+        ref={refOriginImg}
+        quality={100}
+        sizes={`${width ? `(max-width: ${width}px) 100vw, ${width}px` : '100vw'}`}
+        {...props}
+        alt={alt}
+      /> */}
+
       <Image
         ref={refPlaceImg}
         className={`${className} ${s.imagePreload_placeholder}`}
         src={src}
-        onLoad={() => {
-          // onLoaded();
-          setIsReady(true);
-        }}
         width={50}
         height={50}
         alt={`${alt}-placeholder`}
