1b13651aef7db1c1562951ef4844ebfdfe2b2a54
0xd22f9c <dev.hontran@gmail.com>
2024-12-31 17:07:47 +0700

    Refactor preloader management and enhance Image components
    - Updated PageReset component to ensure preloader resets on unmount.
    - Added console logging in useLoader for better tracking of loading progress.
    - Removed commented-out code and improved preloader state management in preloaderSignals.
    - Refactored ImagePlaceHolder and ImagePreload components to streamline preloader registration and unregistration.
    - Enhanced ImagePlaceHolder to dynamically set width and height based on viewport visibility.
    
    This commit improves preloader functionality and optimizes image loading behavior, contributing to a smoother user experience.

--- src/components/ImagePlaceHolder/index.tsx
+++ src/components/ImagePlaceHolder/index.tsx
@@ -1,34 +1,56 @@
 'use client';
 
+import { useSignalEffect } from '@preact/signals-react';
 import Image, { ImageProps } from 'next/image';
-import { ReactElement, useRef } from 'react';
+import { ReactElement, useLayoutEffect, useRef, useState } from 'react';
+
+import { registerPreloader, unRegisterPreloader } from '@/animation/signals/preloaderSignals';
+import { useIsInViewport } from '@/hooks/useIsInViewport';
 
 import s from './style.module.scss';
 
 const ImagePlaceHolder = (props: ImageProps): ReactElement => {
   const refPlaceImg = useRef<HTMLImageElement>(null);
-  // const refOriginImg = useRef<HTMLImageElement>(null);
+  const [width, setWidth] = useState<number>(50);
+  const [height, setHeight] = useState<number>(50);
 
-  const { className, alt, src } = props;
+  const { className, width: widthProp, height: heightProp, alt, src } = props;
+
+  const { visible, kill: killVisible } = useIsInViewport({
+    ref: refPlaceImg,
+  });
+
+  useSignalEffect(() => {
+    if (visible.value) {
+      setWidth(Number(widthProp) ?? 50);
+      setHeight(Number(heightProp) ?? 50);
+      killVisible();
+    }
+  });
+
+  const onLoaded = (): void => {
+    unRegisterPreloader();
+    refPlaceImg.current?.style.setProperty(
+      'aspect-ratio',
+      `${refPlaceImg.current?.width} / ${refPlaceImg.current?.height}`
+    );
+  };
+
+  useLayoutEffect(() => {
+    registerPreloader();
+  }, []);
 
   return (
-    <div className={`${s.imagePreload} imagePreload`}>
-      {/* <Image
-        className={`${className} ${s.imagePreload_origin}`}
-        ref={refOriginImg}
-        quality={100}
-        sizes={`${width ? `(max-width: ${width}px) 100vw, ${width}px` : '100vw'}`}
-        {...props}
-        alt={alt}
-      /> */}
-
+    <div className={`${s.imagePlaceholder} image-placeholder`}>
       <Image
         ref={refPlaceImg}
-        className={`${className} ${s.imagePreload_placeholder}`}
+        className={`${className}`}
         src={src}
-        width={50}
-        height={50}
-        alt={`${alt}-placeholder`}
+        width={width}
+        height={height}
+        alt={`${alt}`}
+        loading="eager"
+        onLoad={onLoaded}
       />
     </div>
   );
