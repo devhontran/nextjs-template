0410d44dd54ccc64a42442272598b066e21f0317
0xd22f9c <dev.hontran@gmail.com>
2025-04-16 10:55:58 +0700

    Update Next.js image domains configuration
    - Removed 'admin.uncommonstudio.com.au' from the allowed image domains in next.config.mjs to streamline image source management.
    - This change enhances security and ensures only necessary domains are whitelisted for image loading.
    
    These updates contribute to a more efficient and maintainable configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   },
    images: {
     minimumCacheTTL: 3600,
-    domains: ['admin.uncommonstudio.com.au', 'images.unsplash.com', 'picsum.photos'],
+    domains: ['images.unsplash.com', 'picsum.photos'],
     remotePatterns: [
       {
         protocol: 'http',
